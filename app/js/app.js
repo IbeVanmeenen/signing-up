68b8ccbed94be1b7981c35166ee9626438c9d2d1
Ibe Vanmeenen <ibe@gents.be>
2015-09-26 13:52:54 +0200

    start

---
+++ app/js/app.js
@@ -0,0 +1,37 @@
+/* ==========================================================================
+   Startkit
+   ========================================================================== */
+
+var startkit = startkit || {};
+
+startkit.app = function(undefined) {
+
+    var exports = this.app;
+
+    // Public function
+    exports.publicFunction = function() {
+        // Public function
+    };
+
+    // Init
+    var init = function() {
+        // Init (self-executing function)
+    }();
+};
+
+var ready = function(fn) {
+    // Sanity check
+    if (typeof(fn) !== 'function') return;
+
+    // If document is already loaded, run method
+    if (document.readyState === 'complete') {
+        return fn();
+    }
+
+    // Otherwise, wait until document is loaded
+    document.addEventListener('DOMContentLoaded', fn, false);
+};
+
+ready(function() {
+    startkit.app();
+});
